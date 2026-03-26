c9f0fa752dc05d0184eba60e8f03553b2d49c4b6
bhuiyr2 <bhuiyr2@grace.cas.mcmaster.ca>
2026-03-25 20:01:27 -0400

    Copied empathic similarity models and established changes.

---
+++ src/LLMPrompt.py
@@ -0,0 +1,4 @@
+# Models Evaluated: Claude 3 Opus, GPT-4o, and Claude 4.5 Sonnet
+# 0-shot and 5-shot prompting was done
+# Prompt template:
+# "You are a psychologist with expertise in analyzing empathy. You can predict how muchpeople might empathize with each other, based on their past experiences. You will receive two stories, one from person A and the other from person B. Please predict, on a scale from 0 to 1, where 0 is not empathetic and 1 is extremely empathetic, how much person A would empathize with story B. Return just the number, no other text."
